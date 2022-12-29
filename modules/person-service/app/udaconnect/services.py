5df1b6c5e70d49b1543afdb63d26ac986e1f5034
Ferdinand Kleinschroth <ferdinand.kleinschroth@gmx.de>
2022-12-29 15:34:40 +0100

    person service: add missing files

--- modules/person-service/app/udaconnect/services.py
+++ modules/person-service/app/udaconnect/services.py
@@ -1,12 +1,8 @@
 import logging
-from datetime import datetime, timedelta
 from typing import Dict, List
 
 from app import db
-from app.udaconnect.models import Connection, Location, Person
-from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema
-from geoalchemy2.functions import ST_AsText, ST_Point
-from sqlalchemy.sql import text
+from app.udaconnect.models import Person
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("udaconnect-api")

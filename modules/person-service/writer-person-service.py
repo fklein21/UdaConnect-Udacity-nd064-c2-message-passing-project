b336777a067b59e2e5768273878c7222bb175f45
Ferdinand Kleinschroth <ferdinand.kleinschroth@gmx.de>
2022-12-29 15:44:29 +0100

    person service: add missing files

--- modules/person-service/writer-person-service.py
+++ modules/person-service/writer-person-service.py
@@ -6,18 +6,3 @@ API_URL = "http://localhost:30010/person"
 result = requests.get(API_URL)
 if result.status_code == 200:
     print(result.json())
-
-# should return the demo path example
-result = requests.post(API_URL + "/demo/myPath?param_demo=myParam", json={"key": "value"}, headers={"header_demo": "myHeader"})
-if result.status_code == 200:
-    print(result.json())
-
-# should return the api example item
-result = requests.get(API_URL + "/api/items/1")
-if result.status_code == 200:
-    print(result.json())
-
-# should return the api example new item
-result = requests.post(API_URL + "/api/items/1", json={"name": "new_item"})
-if result.status_code == 200:
-    print(result.json())

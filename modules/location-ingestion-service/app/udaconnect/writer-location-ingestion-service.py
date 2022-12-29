cf51f324db49d83137ac6a7ddeaf6e1b9a6b9c85
Ferdinand Kleinschroth <ferdinand.kleinschroth@gmx.de>
2022-12-29 12:24:48 +0100

    location db service, push to dockerhub

--- modules/location-ingestion-service/app/udaconnect/writer-location-ingestion-service.py
+++ modules/location-ingestion-service/app/udaconnect/writer-location-ingestion-service.py
@@ -9,7 +9,7 @@ Sample implementation of a writer that can be used to write messages to gRPC.
 
 print("Sending sample payload...")
 
-channel = grpc.insecure_channel("localhost:5555")
+channel = grpc.insecure_channel("localhost:30020")
 stub = location_pb2_grpc.LocationServiceStub(channel)
 
 # Update this with desired payload

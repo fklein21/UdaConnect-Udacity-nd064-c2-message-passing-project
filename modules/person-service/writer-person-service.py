58c6b1b040966a01c07d76ec30e129b482d5b9d2
Ferdinand Kleinschroth <ferdinand.kleinschroth@gmx.de>
2022-12-29 16:10:26 +0100

    person service: delete wsgi files

--- modules/person-service/writer-person-service.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-API_URL = "http://localhost:30010/person"
-
-# should return the default route"s output
-result = requests.get(API_URL)
-if result.status_code == 200:
-    print(result.json())
